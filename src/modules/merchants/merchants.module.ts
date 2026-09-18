7a4547039a6f4d861445933a13bbe6d87346b8c6
FernandoTHS <44153498+fernandoths98@users.noreply.github.com>
2026-09-18 20:29:39 +0700

    feat(merchants): register merchant portal auth

--- src/modules/merchants/merchants.module.ts
+++ src/modules/merchants/merchants.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Merchant } from './entities/merchant.entity';
 import { MerchantsController } from './merchants.controller';
 import { MerchantsAdminController } from './merchants.admin.controller';
+import { MerchantPortalController } from './merchant-portal.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MerchantsService } from './merchants.service';
-@Module({imports:[TypeOrmModule.forFeature([Merchant])],controllers:[MerchantsController,MerchantsAdminController],providers:[MerchantsService]})
+@Module({imports:[TypeOrmModule.forFeature([Merchant]), JwtModule.registerAsync({imports:[ConfigModule],inject:[ConfigService],useFactory:(config:ConfigService)=>({secret:config.getOrThrow<string>('JWT_SECRET'),signOptions:{expiresIn:config.get('JWT_EXPIRES_IN','12h')}})})],controllers:[MerchantsController,MerchantsAdminController],providers:[MerchantsService]})
 export class MerchantsModule {}

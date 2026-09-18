b27c121c3422b6fc13a3e0ea42b92080bcfd2a37
FernandoTHS <44153498+fernandoths98@users.noreply.github.com>
2026-09-18 23:36:06 +0700

    feat(suppliers): wire supplier sync API

--- src/modules/suppliers/suppliers.module.ts
+++ src/modules/suppliers/suppliers.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Supplier } from './entities/supplier.entity';
+import { SupplierSyncRun } from './entities/supplier-sync-run.entity';
+import { Product } from '../products/entities/product.entity';
+import { ProductBatchPrice } from '../products/entities/product-batch-price.entity';
+import { BatchesModule } from '../batches/batches.module';
+import { SupplierSyncService } from './supplier-sync.service';
+import { SupplierSyncController } from './supplier-sync.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Supplier])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Supplier, SupplierSyncRun, Product, ProductBatchPrice]), BatchesModule],
+  controllers: [SupplierSyncController],
+  providers: [SupplierSyncService],
+  exports: [TypeOrmModule, SupplierSyncService],
 })
 export class SuppliersModule {}

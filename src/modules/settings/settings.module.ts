43089dcc89cb3963cfc605f2aa848b0e8f7693f5
FernandoTHS <44153498+fernandoths98@users.noreply.github.com>
2026-09-19 13:53:22 +0700

    feat: register public storefront settings endpoint

--- src/modules/settings/settings.module.ts
+++ src/modules/settings/settings.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
+import { StorefrontSettingsController } from './storefront-settings.controller';
 
 @Module({
-  controllers: [SettingsController],
+  controllers: [SettingsController, StorefrontSettingsController],
   providers: [SettingsService],
   exports: [SettingsService],
 })
